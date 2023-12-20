6153316ab8f188159d1a3d8410c6ae716e082571
이준용 <iiiuooyoo@gmail.com>
2023-12-20 09:51:45 +0900

    feat : react-query 추가 및 close 버튼 스타일 속성 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { TheHeader } from "./layout";
+
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <TheHeader />
             <main>
                 <Outlet />
             </main>
-        </>
+            <ReactQueryDevtools
+                initialIsOpen={false}
+                closeButtonProps={{
+                    style: {
+                        left: "unset",
+                        right: 0,
+                    },
+                }}
+            />
+        </QueryClientProvider>
     );
 }
 
